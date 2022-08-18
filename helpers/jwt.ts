50e86cfc22ba4bc1218ef07207033313d0bb0b1d
Avisheksingh296 <77850562+Avisheksingh296@users.noreply.github.com>
2022-08-18 13:26:57 +0530

    helpers

---
+++ helpers/jwt.ts
@@ -0,0 +1,33 @@
+//@ts-nocheck 
+const expressJwt = require('express-jwt');
+require('dotenv/config');
+function authJwt() {
+    const secret = "I-love-my-dog";
+    const api = process.env.API_URL;
+    return expressJwt({
+        secret,
+        algorithms: ['HS256'],
+        isRevoked: isRevoked
+    }).unless({
+        path: [
+            {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
+            '/api/v1/users/login',
+            '/api/v1/users/register'
+                ]
+    })
+}
+
+async function isRevoked(req, payload, done) {
+    if(!payload.isAdmin) {
+        done(null, true)
+    }
+
+    done();
+}
+
+
+
+module.exports = authJwt
